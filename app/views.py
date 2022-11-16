c6f9e64d21c6d0151470b89656c983542d06a141
David Song <davisong@umich.edu>
2022-11-16 14:12:58 -0500

    :bug: Fix generic view type.

--- app/views.py
+++ app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.views import generic
 
 # Create your views here.
-class IndexView(generic.ListView):
+class IndexView(generic.TemplateView):
   template_name = 'app/index.html'
