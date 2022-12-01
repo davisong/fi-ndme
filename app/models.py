2a378e399df23a03dae3f46122bb6b693b6d6c41
David Song <davisong@umich.edu>
2022-12-01 17:26:06 -0500

    :art: First pass at adding models.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+# Abstract base class to combine with other models
+# DOES NOT HAVE DB TABLE
+
+
+class CommonInfo(models.Model):
+    datetime = models.DateTimeField('date and time')
+    phone_number = models.CharField(max_length=14)
+
+    class Meta:
+        abstract = True
+
+
+class PhoneCall(CommonInfo):
+    id = models.BigAutoField(primary_key=True)
+    CALL_TYPES = [
+        ('RECEIVE', 'Incoming call'),
+        ('SEND', 'Outgoing call'),
+        ('MISS', 'Missed call'),
+    ]
+    type = models.CharField(choices=CALL_TYPES, max_length=13)
+    duration = models.PositiveIntegerField()  # range is 0 to 2147483647
+
+    class Meta:
+        db_table = 'calls'
+
+
+class TextMessage(CommonInfo):
+    id = models.BigAutoField(primary_key=True)
+    MESSAGE_TYPES = [
+        ('RECEIVE', 'Received text'),
+        ('SEND', 'Sent text'),
+    ]
+    type = models.CharField(choices=MESSAGE_TYPES, max_length=13)
+
+    class Meta:
+        db_table = 'messages'
